c64c4d74850f5ff188a3a44dc7621f9b84176536
Akhshaiya S <sakhshaiya01@gmail.com>
2025-12-16 18:38:31 +0530

    Update C-Level4.c

--- C-Level4.c
+++ C-Level4.c
@@ -252,3 +252,63 @@ print:
     printf("%d\n", result);
     return 0;
 }
+//Program 14
+#include <stdio.h>
+int main()
+{
+    int x, temp, last, msb = 0, place = 1, result = 0;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    temp = x;
+    last = x % 10;
+
+    if (last % 2 == 0)
+    {
+        printf("%d\n", x);
+        goto end;
+    }
+
+find:
+    if (temp >= 10)
+    {
+        temp = temp / 10;
+        place = place * 10;
+        goto find;
+    }
+    else
+    {
+        msb = temp;
+    }
+    msb = msb - 1;
+    result = msb * place + (x % place);
+    printf("%d\n", result);
+
+end:
+    return 0;
+}
+//Program 15
+#include <stdio.h>
+int main()
+{
+    int n, i, flag = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n <= 1)
+    {
+        printf("Not Prime\n");
+        return 0;
+    }
+    for (i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (flag == 0)
+        printf("Prime\n");
+    else
+        printf("Not Prime\n");
+    return 0;
+}
