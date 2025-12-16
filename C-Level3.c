211094446ac74317b21514eba7c0277c1e7c1252
Akhshaiya S <sakhshaiya01@gmail.com>
2025-12-16 14:05:03 +0530

    Create C-Level3.c

---
+++ C-Level3.c
@@ -0,0 +1,220 @@
+//Program 1
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    if (x==50){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 2
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    if (x!=50){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 3
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    if (x<50){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 4
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    if (x>50){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 5
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    if (x<=50){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 6
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    if (x>=50){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 7
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    int a,b;
+    a = x/10;
+    b = x%10;
+    if (a==b){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 8
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    int a,b;
+    a = x/10;
+    b = x%10;
+    if (a!=b){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 9
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    int a,b;
+    a = x/10;
+    b = x%10;
+    if (a>b){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 10
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    int a,b;
+    a = x/10;
+    b = x%10;
+    if (a<b){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 11
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    int a,b;
+    a = x/10;
+    b = x%10;
+    if (a<=b){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 12
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    int a,b;
+    a = x/10;
+    b = x%10;
+    if (a>=b){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 13
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    int a,b;
+    a = (x%100)/10;
+    b = x%10;
+    if (a==b){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
+//Program 14
+#include <stdio.h>
+int main ()
+{
+    int x,y;
+    printf("Enter number: ");
+    scanf("%d",&x);
+    int a,b;
+    a = (x/100);
+    b = x%100;
+    if (a==b){
+        printf("Success");
+    }
+    else{
+        printf("Failure");
+    }
+}
